857e9ca41bf9439476fa288625d75ecebd3135f4
hojay <jeahonglee@gmail.com>
2024-09-05 15:07:14 +0900

    course banner image upload and save in firebase and db ready

--- app/create-course/[courseId]/_components/CourseBasicInfo.jsx
+++ app/create-course/[courseId]/_components/CourseBasicInfo.jsx
@@ -2,8 +2,43 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { HiOutlinePuzzlePiece } from 'react-icons/hi2';
 import EditCourseBasicInfo from './EditCourseBasicInfo';
+import { useState } from 'react';
+import { storage } from '@/configs/firebaseConfig';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { db } from '@/configs/db';
+import { CourseList } from '@/configs/schema';
+import { eq } from 'drizzle-orm';
 
 function CourseBasicInfo({ course, refreshData }) {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  /**
+   * Upload image to firebase storage
+   * @param {File} file
+   */
+
+  const onFileSelected = async (e) => {
+    const file = e.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+    const fileName = Date.now() + '.jpg';
+    const storageRef = ref(storage, 'ai-course/' + fileName);
+    await uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log('Uploaded a file!');
+      })
+      .then((resp) => {
+        getDownloadURL(storageRef).then(async (downloadUrl) => {
+          console.log(downloadUrl);
+          await db
+            .update(CourseList)
+            .set({
+              courseBanner: downloadUrl,
+            })
+            .where(eq(CourseList.id, course?.id));
+        });
+      });
+  };
+
   return (
     <div className="p-10 border rounded-xl shadow-sm mt-5">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
@@ -22,12 +57,20 @@ function CourseBasicInfo({ course, refreshData }) {
           <Button className="w-full mt-5">Start</Button>
         </div>
         <div>
-          <Image
-            src={'/logo.svg'}
-            alt="logo"
-            width={300}
-            height={300}
-            className="w-full border rounded-xl h-[250px] object-cover"
+          <label htmlFor="upload-image">
+            <Image
+              src={selectedImage || '/logo.svg'}
+              alt="logo"
+              width={300}
+              height={300}
+              className="w-full border rounded-xl h-[250px] object-cover cursor-pointer"
+            />
+          </label>
+          <input
+            type="file"
+            id="upload-image"
+            className="hidden"
+            onChange={onFileSelected}
           />
         </div>
       </div>
---
+++ configs/firebaseConfig.jsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getStorage } from 'firebase/storage';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: 'next-ai-course-generator.firebaseapp.com',
+  projectId: 'next-ai-course-generator',
+  storageBucket: 'next-ai-course-generator.appspot.com',
+  messagingSenderId: '500987348169',
+  appId: '1:500987348169:web:a4913df06dfc36fd69b918',
+  measurementId: 'G-K19T7N8PMJ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const storage = getStorage(app);
